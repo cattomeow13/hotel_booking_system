99a024a16942a4bf73a3159e87172f00a65d667d
cattomeow13 <157349174+cattomeow13@users.noreply.github.com>
2024-01-29 12:47:21 +0500

    "Files are Added"

---
+++ hotel_room.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <>time>
+usimg namespace std;
+
+public class Hotel_Room{
+	
+	void room_types()
+	{
+	}
+	void prices()
+	{
+	}
+	void amenities()
+	{
+	}
+	void availability()
+	{
+	}
+};
+class search_hotel{
+	
+	string hotel_name();
+	string location();
+	date date();
+}; 
+class staff()
+{
+	void availability()
+	void booking()
+	
+}
